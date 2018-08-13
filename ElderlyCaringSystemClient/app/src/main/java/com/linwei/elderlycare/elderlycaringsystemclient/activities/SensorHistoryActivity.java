26fe54f4a7b61cba10a2514f36ed4f9d55d9d51d
hlwhl <h2113191@gmail.com>
2018-08-13 14:17:14 +0100

    数据可视化优化

--- ElderlyCaringSystemClient/app/src/main/java/com/linwei/elderlycare/elderlycaringsystemclient/activities/SensorHistoryActivity.java
+++ ElderlyCaringSystemClient/app/src/main/java/com/linwei/elderlycare/elderlycaringsystemclient/activities/SensorHistoryActivity.java
@@ -1,12 +1,14 @@
 package com.linwei.elderlycare.elderlycaringsystemclient.activities;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Toast;
 
 import com.linwei.elderlycare.elderlycaringsystemclient.R;
@@ -14,27 +16,41 @@ import com.linwei.elderlycare.elderlycaringsystemclient.adapters.HistoryDataAdap
 import com.linwei.elderlycare.elderlycaringsystemclient.entities.Sensor;
 import com.linwei.elderlycare.elderlycaringsystemclient.entities.SensorDataHistory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import cn.bmob.v3.BmobQuery;
 import cn.bmob.v3.exception.BmobException;
 import cn.bmob.v3.listener.FindListener;
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.view.LineChartView;
 
 public class SensorHistoryActivity extends AppCompatActivity {
     private RecyclerView recy_his;
     private HistoryDataAdapter adapter;
     private SwipeRefreshLayout swipeRefreshLayout;
     private Sensor sensor;
+    private LineChartView chart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor_history);
-        recy_his = findViewById(R.id.his_list);
-        swipeRefreshLayout = findViewById(R.id.refresh_his);
+
 
         Intent intent = getIntent();
         sensor = (Sensor) intent.getSerializableExtra("sensor");
+        if (sensor.getTitle().equals("Temperature")) {
+            chart = findViewById(R.id.linechart_temp);
+            chart.setVisibility(View.VISIBLE);
+            chart.setInteractive(false);
+        }
+
+        recy_his = findViewById(R.id.his_list);
+        swipeRefreshLayout = findViewById(R.id.refresh_his);
         getSupportActionBar().setTitle(sensor.getTitle() + " History Data");
 
 
@@ -51,11 +67,29 @@ public class SensorHistoryActivity extends AppCompatActivity {
                     recy_his.setLayoutManager(linearLayoutManager);
                     recy_his.setItemAnimator(new DefaultItemAnimator());
                     recy_his.setAdapter(adapter);
+
+
+                    //Set temp chart
+                    if (sensor.getTitle().equals("Temperature")) {
+                        List<PointValue> values = new ArrayList<PointValue>();
+                        for (int i = 0; i < list.size(); i++) {
+                            values.add(new PointValue(i, Integer.parseInt(list.get(i).getContent())));
+                        }
+                        Line line = new Line(values).setColor(Color.BLUE).setCubic(true);
+                        List<Line> lines = new ArrayList<Line>();
+                        lines.add(line);
+                        LineChartData data = new LineChartData();
+                        data.setLines(lines);
+                        //set axis
+                        setChartAxis(data);
+                        chart.setLineChartData(data);
+                    }
                 } else {
                     Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
                 }
             }
         });
+
 
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
@@ -79,5 +113,18 @@ public class SensorHistoryActivity extends AppCompatActivity {
         });
     }
 
+    public void setChartAxis(LineChartData data) {
+        //Axis x
+        Axis x = new Axis();
+        //设置斜体
+        x.setHasTiltedLabels(true);
+        x.setTextColor(Color.BLACK);
+        data.setAxisXBottom(x);
 
+        //Axis y
+        Axis y = new Axis();
+        y.setName("Temp ℃");
+        y.setTextColor(Color.BLACK);
+        data.setAxisYLeft(y);
+    }
 }
